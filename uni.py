83dff4ce27ff8da26b5ae9bd769c81b81e522450
bobwxc_MI <bobwxc@yeah.net>
2022-05-24 23:40:54 +0800

    solve inline section title reference for pdf/epub

--- uni.py
+++ uni.py
@@ -4,6 +4,19 @@ import re
 import sys
 
 
+def get_first_anchor(path):
+    f = open(path)
+    while(1):
+        l = f.readline()
+        if not l:
+            break
+        elif re.match("#+ ", l):
+            f.close()
+            return '#' + (l[l.find(' ')+1:-1]).lower().replace(' ', '-')
+    f.close()
+    return '#'
+
+
 def uni_file(path, n=0):
     f = open(path)
     content = f.readlines()
@@ -16,6 +29,16 @@ def uni_file(path, n=0):
             ref_path = ref_path[1:-1]
             new_content += ['\n']
             new_content += uni_file(ref_path, n+1)
+        elif re.search("\[.+\]\(.+\.md\)", i):
+            ref = re.search("\[.+\]\(.+\.md\)", i)
+            start = ref.start()
+            end = ref.end()
+            ref_path = ref.group()
+            ref_path = ref_path[ref_path.find('(')+1:-1]
+            if path.rfind('/') != -1:
+                ref_path = path[:path.rfind('/')] + '/' + ref_path
+            new_line = i[:i.find('(', start)+1]+get_first_anchor(ref_path)+i[end-1:]
+            new_content.append(new_line)
         else:
             new_content.append(i)
     f.close()
